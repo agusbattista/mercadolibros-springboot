7f063574dc75c4435e1e7590cdd681bf0f6bb3db
Agustín Battista <agusbattista@gmail.com>
2025-09-30 16:21:32 -0300

    initial commit

---
+++ src/main/java/io/github/agusbattista/mercadolibros_springboot/MercadolibrosSpringbootApplication.java
@@ -0,0 +1,13 @@
+package io.github.agusbattista.mercadolibros_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadolibrosSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadolibrosSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/agusbattista/mercadolibros_springboot/MercadolibrosSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.agusbattista.mercadolibros_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolibrosSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
