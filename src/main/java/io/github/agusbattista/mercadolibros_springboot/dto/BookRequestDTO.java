4cdf504b6cdf3f274948d0ce710bf3e969dcfd3e
Agustín Battista <agusbattista@gmail.com>
2026-01-15 17:17:22 -0300

    chore(dto): fix spelling error in comments

--- src/main/java/io/github/agusbattista/mercadolibros_springboot/dto/BookRequestDTO.java
+++ src/main/java/io/github/agusbattista/mercadolibros_springboot/dto/BookRequestDTO.java
@@ -11,7 +11,7 @@ import org.hibernate.validator.constraints.URL;
 
 public record BookRequestDTO(
     @NotNull(message = "El ISBN es obligatorio")
-        // type = ANY acepta ISBN-10 y ISBN-13
+        // type = ANY acepta ISBN-10 o ISBN-13
         @ISBN(type = ISBN.Type.ANY, message = "El formato del ISBN no es válido")
         String isbn,
     @NotBlank(message = "El título es obligatorio")
