337928ce75aeedf903edb90c9c2c578b59a3ec51
Agustín Battista <agusbattista@gmail.com>
2026-01-15 23:52:51 -0300

    test: add application context smoke test

--- src/test/java/io/github/agusbattista/mercadolibros_springboot/MercadolibrosSpringbootApplicationTests.java
+++ src/test/java/io/github/agusbattista/mercadolibros_springboot/MercadolibrosSpringbootApplicationTests.java
@@ -1,11 +1,19 @@
 package io.github.agusbattista.mercadolibros_springboot;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.github.agusbattista.mercadolibros_springboot.controller.BookController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MercadolibrosSpringbootApplicationTests {
 
+  @Autowired private BookController controller;
+
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    assertThat(controller).isNotNull();
+  }
 }
